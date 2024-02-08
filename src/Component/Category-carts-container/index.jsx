665f022bc035069761e6af5b0b05b496fd408937
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-02-08 12:32:53 +0500

    add some change

--- src/Component/Category-carts-container/index.jsx
+++ src/Component/Category-carts-container/index.jsx
@@ -4,15 +4,15 @@ import Carts from '../Carts';
 function CategoryCartsContainer({ products, category }) {
 
     const res = products.filter(res => res?.category == category);
-    
-const category1stletter = category[0].toUpperCase();
-const categorywithout1stletter = category.slice(1);
+
+    const category1stletter = category[0].toUpperCase();
+    const categorywithout1stletter = category.slice(1);
 
     return (
         <div style={{ width: '100%' }}>
-            <h1 style={{ textAlign: 'left', marginLeft: 11 }}>{category1stletter+categorywithout1stletter}</h1>
+            <h1 style={{ textAlign: 'left', marginLeft: 19 }}>{category1stletter + categorywithout1stletter}</h1>
 
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div style={{ display: 'flex', justifyContent:'center', flexWrap: 'wrap' }}>
                 {res.map((element, id) => {
                     return <Carts key={id} cartInfo={element} />
                 })}
